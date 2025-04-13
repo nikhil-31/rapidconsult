118bec55d96e1b5b9c3eeb5e5521e616488f8b7c
Nikhil B <nkl07ba@gmail.com>
2025-04-14 04:12:14 +0530

    Initial message

---
+++ rapidconsult/rapidconsult/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from rapidconsult.users.models import User
+from rapidconsult.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
