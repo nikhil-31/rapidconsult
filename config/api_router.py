49633049c823870471ff686cdd1c4820e7b10cd1
Nikhil B <nkl07ba@gmail.com>
2025-06-21 20:01:51 +0530

    fixed import issues while running daphne server

--- config/api_router.py
+++ config/api_router.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from rest_framework.routers import DefaultRouter
 from rest_framework.routers import SimpleRouter
 
-from chats.api.views import ConversationViewSet, MessageViewSet
+from rapidconsult.chats.api.views import ConversationViewSet, MessageViewSet
 from rapidconsult.users.api.views import UserViewSet
 
 router = DefaultRouter() if settings.DEBUG else SimpleRouter()
--- config/asgi.py
+++ config/asgi.py
@@ -9,20 +9,26 @@ https://docs.djangoproject.com/en/dev/howto/deployment/asgi/
 """
 import os
 import sys
+import django
 from pathlib import Path
 
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, ROOT_DIR)
+
+
+# If DJANGO_SETTINGS_MODULE is unset, default to the local settings
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+
+django.setup()
+
 from django.core.asgi import get_asgi_application
-from chats.middleware import TokenAuthMiddleware
+from rapidconsult.chats.middleware import TokenAuthMiddleware
 
 # This allows easy placement of apps within the interior
 # rapidconsult directory.
 
 # ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent
 # sys.path.append(str(ROOT_DIR / "rapidconsult"))
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-# If DJANGO_SETTINGS_MODULE is unset, default to the local settings
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 
 # This application object is used by any ASGI server configured to use this file.
 django_application = get_asgi_application()
@@ -31,7 +37,6 @@ django_application = get_asgi_application()
 from config import routing  # noqa isort:skip
 
 from channels.routing import ProtocolTypeRouter, URLRouter  # noqa isort:skip
-
 
 application = ProtocolTypeRouter(
     {
