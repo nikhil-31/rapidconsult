0a9262b34dd3a78899b10ca6b937e3237927cd31
Nikhil B <nkl07ba@gmail.com>
2025-06-27 22:52:41 +0530

    MOVED CORS_ALLOWED_ORIGINS to environment

--- config/settings/base.py
+++ config/settings/base.py
@@ -364,8 +364,4 @@ CHANNEL_LAYERS = {
     },
 }
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "https://api.crystalmelting.com",
-    "https://rapidconsult.netlify.app",
-]
+CORS_ALLOWED_ORIGINS = env.list("DJANGO_CORS_ALLOWED_ORIGINS", default=["*"])
