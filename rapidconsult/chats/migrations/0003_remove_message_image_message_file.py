6748a24a73853fd248ae976a51a03872ccee9db1
Nikhil B <nkl07ba@gmail.com>
2025-06-29 15:20:50 +0530

    Added migrations for file field

--- rapidconsult/chats/migrations/0003_remove_message_image_message_file.py
+++ rapidconsult/chats/migrations/0003_remove_message_image_message_file.py
@@ -10,6 +10,10 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RemoveField(
+            model_name='message',
+            name='image',
+        ),
         migrations.AddField(
             model_name='message',
             name='file',
