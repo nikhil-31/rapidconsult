bc2cd044522b945b3df71410ac46e400964aeea2
Nikhil B <nkl07ba@gmail.com>
2025-07-16 12:42:53 +0530

    Updated Navbar to use correct tabs

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -10,107 +10,66 @@ export function Navbar() {
 
     return (
         <>
-            <nav className="bg-white border-gray-200 px-4 sm:px-6 py-2.5 rounded dark:bg-gray-800">
+            <nav className="bg-white shadow-md px-4 sm:px-6 py-2.5 rounded mb-6">
                 <div className="max-w-5xl mx-auto flex flex-wrap justify-between items-center">
                     <Link to="/" className="flex items-center">
-            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-              RapidConsult
-            </span>
+        <span className="self-center text-xl font-semibold whitespace-nowrap text-black">
+          RapidConsult
+        </span>
                     </Link>
                     <button
                         data-collapse-toggle="mobile-menu"
                         type="button"
-                        className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                        className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
                         aria-controls="mobile-menu"
                         aria-expanded="false"
                     >
                         <span className="sr-only">Open main menu</span>
-                        <svg
-                            className="w-6 h-6"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                fillRule="evenodd"
-                                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                                clipRule="evenodd"
-                            ></path>
-                        </svg>
-                        <svg
-                            className="hidden w-6 h-6"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                fillRule="evenodd"
-                                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                clipRule="evenodd"
-                            ></path>
-                        </svg>
+                        {/* ...hamburger and close icons... */}
                     </button>
-                    <div className="hidden w-full md:block md:w-auto" id="mobile-menu">
-                        <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+                    <div className="hidden w-full md:block md:w-auto text-center" id="mobile-menu">
+                        <ul className="flex flex-col items-center mt-4 md:flex-row md:justify-center md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+                            <li><Link to="/" className="block py-2 pr-4 pl-3 text-black md:p-0">Schedules</Link></li>
+                            <li><Link to="/" className="block py-2 pr-4 pl-3 text-black md:p-0">On-Call</Link></li>
+                            <li><Link to="/" className="block py-2 pr-4 pl-3 text-black md:p-0">Messages</Link></li>
+                            <li><Link to="/" className="block py-2 pr-4 pl-3 text-black md:p-0">Contacts</Link></li>
+                            {/*                  <li>*/}
+                            {/*                      <Link to="/conversations" className="block py-2 pr-4 pl-3 text-black md:p-0">*/}
+                            {/*                          Contacts*/}
+                            {/*                          {unreadMessageCount > 0 && (*/}
+                            {/*                              <span*/}
+                            {/*                                  className="ml-2 inline-flex items-center justify-center h-6 w-6 rounded-full bg-white">*/}
+                            {/*  <span className="text-xs font-medium leading-none text-black">*/}
+                            {/*    {unreadMessageCount}*/}
+                            {/*  </span>*/}
+                            {/*</span>*/}
+                            {/*                          )}*/}
+                            {/*                      </Link>*/}
+                            {/*                  </li>*/}
                             <li>
-                                <Link
-                                    to="/"
-                                    className="block py-2 pr-4 pl-3 text-white md:p-0 dark:text-white"
-                                    aria-current="page"
-                                >
-                                    Chats
-                                </Link>
-                            </li>
-                            <li>
-                                <Link
-                                    to="/conversations"
-                                    className="block py-2 pr-4 pl-3 text-white md:p-0 dark:text-white"
-                                    aria-current="page"
-                                >
-                                    Active Conversations
-                                    {unreadMessageCount > 0 && (
+                                {!user ? (
+                                    <Link to="/login" className="block py-2 pr-4 pl-3 text-black md:p-0">Login</Link>
+                                ) : (
+                                    <>
                                         <span
-                                            className="ml-2 inline-flex items-center justify-center h-6 w-6 rounded-full bg-white"><span
-                                            className="text-xs font-medium leading-none text-gray-800">
-                                            {unreadMessageCount}
-                                          </span>
-                                        </span>
-                                    )}
-                                </Link>
-                            </li>
-                            <li>
-                                {/*<Link*/}
-                                {/*    to="/login"*/}
-                                {/*    className="block py-2 pr-4 pl-3 text-white md:p-0 dark:text-white"*/}
-                                {/*>*/}
-                                {/*    Login*/}
-                                {/*</Link>*/}
-
-                                {
-                                    !user ? (
-
-                                        <Link to="/login"
-                                              className="block py-2 pr-4 pl-3 text-white md:p-0 dark:text-white">
-                                            Login
-                                        </Link>
-
-                                    ) : (
-                                        <>
-                                            <span className="text-white">Logged in: {user.username}</span>
-                                            <button className="block py-2 pr-4 pl-3 text-white md:p-0 dark:text-white"
-                                                    onClick={logout}>
-                                                Logout
-                                            </button>
-                                        </>
-                                    )
-                                }
-
+                                            className="text-black block py-2 pr-4 pl-3 md:p-0">Logged in: {user.username}</span>
+                                        <button
+                                            className="block py-2 pr-4 pl-3 text-black md:p-0"
+                                            onClick={logout}
+                                        >
+                                            Logout
+                                        </button>
+                                    </>
+                                )}
                             </li>
                         </ul>
                     </div>
+
                 </div>
             </nav>
-            <div className="max-w-5xl mx-auto py-6">
+
+            {/* Main content area with adequate spacing */}
+            <div className="max-w-5xl mx-auto py-6 bg-white text-black">
                 <Outlet/>
             </div>
         </>
