5dd2469744915695da0d8c43e1e04fa5981a067d
Nikhil B <nkl07ba@gmail.com>
2025-09-05 20:30:44 +0530

    Fix for pagination support

--- config/settings/base.py
+++ config/settings/base.py
@@ -373,7 +373,7 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
     # "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 20,
+    # "PAGE_SIZE": 20,
 }
 
 # django-cors-headers - https://github.com/adamchainz/django-cors-headers#setup
