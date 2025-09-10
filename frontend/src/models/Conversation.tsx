69a539b9c4bacad013d8bc45f49cd294060875cd
Nikhil B <nkl07ba@gmail.com>
2025-09-10 12:31:42 +0530

    Removed unused code

--- frontend/src/models/Conversation.tsx
+++
@@ -1,9 +0,0 @@
-import {MessageModel} from "./MessageModel";
-import {UserModel} from "./UserModel";
-
-export interface ConversationModel {
-    id: string;
-    name: string;
-    last_message: MessageModel | null;
-    other_user: UserModel;
-}
